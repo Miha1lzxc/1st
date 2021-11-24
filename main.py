9b6a35921f877010f82b4e611942fd269d285260
Администратор <medved1996misha@gmail.com>
2021-11-24 17:35:06 +0300

    first commit

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-
-Form, Window = uic.loadUiType(" UI.ui")
+from random import randint
 
 
-class Window(QMainWindow):
-    def __init__(self):
-        self.should_paint_circle = False
+class Test(QMainWindow):
+        def __init__(self):
+            super().__init__()
+            top = 400
+            left = 400
+            width = 1500
+            height = 700
 
-    self.btn.clicked.connect(self.paintcircle)
+            thing = "things/test.png"
 
-    def paintEvent(self, event):
-        super().paintEvent(event)
-        if self.should_paint_circle:
-            painter = QtGui.QPainter(self)
-            painter.setRenderHint(QPainter.Antialiasing)
-            painter.setPen(QPen(Qt.yellow, 5, Qt.SolidLine))
-            painter.drawEllipse(100, 100, 100, 100)
+            self.setWindowTitle('number3')
+            self.setGeometry(top, left, width, height)
+            self.setWindowIcon(QIcon(thing))
 
-    def paintcircle(self):
-        self.should_paint_circle = True
+            self.image = QImage(self.size(), QImage.Format_RGB32)
+            self.image.fill(Qt.white)
+
+        self.pushButton.setFixedSize(100, 100)
+        self.pushButton.clicked.connect(self.circle)
+
+        self.label = QLabel()
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def circle(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w, h = [randint(10, 100) for i in range(2)]
+        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
         self.update()
 
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec()
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
