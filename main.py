19543cac86660c01a9489b01ccdf72a1dd93091a
Администратор <medved1996misha@gmail.com>
2021-11-24 17:30:49 +0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType(" UI.ui")
 
 
-if __name__ == '__main__':
-    main()
+class Window(QMainWindow):
+    def __init__(self):
+        self.should_paint_circle = False
+
+    self.btn.clicked.connect(self.paintcircle)
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.should_paint_circle:
+            painter = QtGui.QPainter(self)
+            painter.setRenderHint(QPainter.Antialiasing)
+            painter.setPen(QPen(Qt.yellow, 5, Qt.SolidLine))
+            painter.drawEllipse(100, 100, 100, 100)
+
+    def paintcircle(self):
+        self.should_paint_circle = True
+        self.update()
+
+
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec()
